773db7a3f5972f31d0e0768a63491f6e4b06e65c
Ryan <thewaronryan@gmail.com>
2017-01-02 20:03:37 -0700

    Adding a simple recycler view adapter that MultiViewHolderAdapter extends

--- rapidocommon/src/main/java/com/izeni/rapidocommon/ItemAdapter.kt
+++ rapidocommon/src/main/java/com/izeni/rapidocommon/ItemAdapter.kt
@@ -1,48 +1,39 @@
 package com.izeni.rapidocommon
 
-import android.support.annotation.LayoutRes
 import android.support.v7.widget.RecyclerView
-import android.util.SparseArray
 import android.view.View
 import android.view.ViewGroup
 
 /**
  * Created by ner on 1/2/17.
  */
-class ItemAdapter(val items: MutableList<AdapterItem<Any>>, vararg viewHolders: ViewHolderData) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+abstract class ItemAdapter<T>(val items: MutableList<T>) : RecyclerView.Adapter<ItemAdapter.ViewHolder<T>>() {
 
-    private val holders = SparseArray<ViewHolderData>()
+    abstract override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<T>
 
-    init {
-        viewHolders.forEach { holders.put(it.layoutId, it) }
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        return items[position].viewHolderId
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return holders[viewType]!!.createViewHolder(parent)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder<T>, position: Int) {
         holder.bind(items[position])
     }
 
-    override fun getItemCount() = items.size
-
-    abstract class AdapterItem<out T>(val data: T) {
-        abstract val viewHolderId: Int
+    override fun getItemCount(): Int {
+        return items.size
     }
 
-    class ViewHolderData(@LayoutRes val layoutId: Int, val viewHolder: (View) -> ViewHolder) {
-
-        fun createViewHolder(parent: ViewGroup): ViewHolder {
-            return viewHolder(parent.inflate(layoutId))
-        }
+    abstract class ViewHolder<in T>(view: View) : RecyclerView.ViewHolder(view) {
+        abstract fun bind(item: T)
     }
 
-    abstract class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        abstract fun bind(item: AdapterItem<Any>)
+    fun addItem(item: T) {
+        items.add(item)
+
+        notifyItemChanged(items.size - 1)
+    }
+
+    fun addItems(newItems: List<T>) {
+        val startPosition = items.size + 1
+
+        items.addAll(newItems)
+
+        notifyItemRangeChanged(startPosition, newItems.size)
     }
 }

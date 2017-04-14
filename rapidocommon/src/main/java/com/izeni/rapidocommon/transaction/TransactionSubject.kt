9b8c95c724def3417d83714ff32f83a55688793d
Ryan <thewaronryan@gmail.com>
2017-04-14 10:32:59 -0600

    Removing CompleteOrError and Single Transaction classe, Adding subscribeOnIo and subscribeOnComputation methods, adding doOnSubscribe

--- rapidocommon/src/main/java/com/izeni/rapidocommon/transaction/TransactionSubject.kt
+++ rapidocommon/src/main/java/com/izeni/rapidocommon/transaction/TransactionSubject.kt
@@ -3,18 +3,26 @@ package com.izeni.rapidocommon.transaction
 import com.izeni.rapidocommon.filterNetworkErrors
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
 
 /**
  * Created by ner on 2/28/17.
  */
 open class TransactionSubject<T, P> {
+    private val DEFAULT = 0
+    private val IO = 1
+    private val COMPUTATION = 2
+
     protected val subject: BehaviorSubject<Transaction<T, P>> = BehaviorSubject.create()
 
     private var doOnIdle: (() -> Unit)? = null
     private var doOnLoading: ((P?) -> Unit)? = null
     private var doOnSuccess: ((T) -> Unit)? = null
     private var doOnError: ((Error) -> Unit)? = null
+    private var doOnSubscribe: (() -> Unit)? = null
+
+    private var subscribeOn = DEFAULT
 
     open fun onIdle() {
         if (!subject.hasComplete()) {
@@ -64,34 +72,44 @@ open class TransactionSubject<T, P> {
         doOnSuccess = block
     }
 
+    fun doOnSubscribe(block: () -> Unit): TransactionSubject<T, P> {
+        this.doOnSubscribe = block
+
+        return this
+    }
+
+    fun subscribeOnIo() : TransactionSubject<T, P> {
+        subscribeOn = IO
+
+        return this
+    }
+
+    fun subscribeOnComputation() : TransactionSubject<T, P> {
+        subscribeOn = COMPUTATION
+
+        return this
+    }
+
     fun subject(): Observable<Transaction<T, P>> {
-        return subject.filterNetworkErrors()
+        val observable = subject.filterNetworkErrors()
+                .doOnSubscribe { doOnSubscribe?.invoke() }
+
+        when (subscribeOn) {
+            IO -> return observable.subscribeOn(Schedulers.io())
+            COMPUTATION -> return observable.subscribeOn(Schedulers.computation())
+            else -> return observable
+        }
     }
 
     fun mainThreadSubject(): Observable<Transaction<T, P>> {
-        return subject.filterNetworkErrors()
+        val observable = subject.filterNetworkErrors()
                 .observeOn(AndroidSchedulers.mainThread())
-    }
-}
+                .doOnSubscribe { doOnSubscribe?.invoke() }
 
-class SingleTransactionSubject<T, P> : TransactionSubject<T, P>() {
-    override fun onSuccess(item: T) {
-        super.onSuccess(item)
-
-        subject.onComplete()
-    }
-}
-
-class CompleteOrErrorTransactionSubject<T, P> : TransactionSubject<T, P>() {
-    override fun onSuccess(item: T) {
-        super.onSuccess(item)
-
-        subject.onComplete()
-    }
-
-    override fun onError(error: Error) {
-        super.onError(error)
-
-        subject.onComplete()
+        when (subscribeOn) {
+            IO -> return observable.subscribeOn(Schedulers.io())
+            COMPUTATION -> return observable.subscribeOn(Schedulers.computation())
+            else -> return observable
+        }
     }
 }
